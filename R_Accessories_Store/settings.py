c9aa126447c7eabf159e151946f33dc4d30ddfff
RahmatUllahSA <rahmatullah.iubat.cse@gmail.com>
2025-05-08 15:20:37 +0600

    Create template folder inside of home apps

--- R_Accessories_Store/settings.py
+++ R_Accessories_Store/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFIES_DIRS =[BASE_DIR/'home/templates/static'] #template e static files directory
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
